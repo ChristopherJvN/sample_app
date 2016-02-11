df1f0ac20c66221262b862595ebc9e6546d8038a
Chrisjvn <christopher@kizzler.com>
2016-02-11 11:05:02 +0200

    ability to log in and log out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,9 +6,15 @@ class SessionsController < ApplicationController
     email_address = params[:session][:email_address]
     @user = User.find_by_email_address(email_address)
     if @user.nil?
+      flash.now[:danger] = 'user name or password is incorrect'
       render 'new'
     else
+      log_in @user
       redirect_to @user
     end
+  end
+  def destroy
+    log_out
+    render 'new'
   end
 end
