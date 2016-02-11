16842d4e92d87c730b3be8b6d1cb4aebcf093f90
Chrisjvn <christopher@kizzler.com>
2016-02-11 11:45:34 +0200

    ability to log in and log out with password check

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,16 +3,16 @@ class SessionsController < ApplicationController
   end
 
   def create
-    email_address = params[:session][:email_address]
-    @user = User.find_by_email_address(email_address)
-    if @user.nil?
-      flash.now[:danger] = 'user name or password is incorrect'
-      render 'new'
-    else
-      log_in @user
-      redirect_to @user
+      @user = User.find_by(email_address: params[:session][:email_address].downcase)
+    if @user && @user.authenticate(params[:session][:password])
+        log_in @user
+        redirect_to @user
+      else
+        flash.now[:danger] = 'user name or password is incorrect'
+        render 'new'
     end
   end
+
   def destroy
     log_out
     render 'new'
