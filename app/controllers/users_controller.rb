a120cd829273a5a697cb63e0bdaa366be45896c0
Chrisjvn <christopher@kizzler.com>
2016-02-12 09:14:16 +0200

    added users index text, can link to users name

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
   def new
     @user = User.new
   end
@@ -7,10 +8,7 @@ class UsersController < ApplicationController
     @users = User.paginate(page: params[:page])
   end
 
-  # PATCH/PUT /users/1
-  # PATCH/PUT /users/1.json
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:success] = 'User updated'
       redirect_to @user
@@ -20,25 +18,19 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
-  # # DELETE /users/1
-  # # DELETE /users/1.json
   def destroy
-    @user = User.find(params[:id])
     @user.destroy
-  #   User.find(params[:id]).destroy
-   flash[:success] = "User deleted"
-   redirect_to users_url
+    flash[:success] = 'User deleted'
+    redirect_to users_url
    end
 
   def show
-    @user = User.find(params[:id])
   end
 
   def create
-    @user = User.new(user_params) # Not the final implementation!
+    @user = User.new(user_params)
     if @user.save
       flash[:success] = 'Welcome to the Sample App!'
       redirect_to @user
@@ -49,5 +41,9 @@ class UsersController < ApplicationController
   private def user_params
     params.require(:user).permit(:name, :email_address, :password,
                                  :password_confirmation)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
